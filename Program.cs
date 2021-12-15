df95c0dceb8abc5ec70058cf4969f06c0c10e122
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-15 11:54:33 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿int firstNumber = 2;
+int secondNumber = 3;
+int thirdNumber = 8;
+Console.WriteLine($"Voici les trois nombres : {firstNumber}, {secondNumber}, {thirdNumber}");
+int operation = (2+3)*8;
+Console.WriteLine($"Si on fait l'opération suivante ({firstNumber}+{secondNumber})*{thirdNumber}, on obtient {operation}.");
